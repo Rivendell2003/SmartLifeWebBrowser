9534418c8769b17d9816843f7b14df851e5660f2
Jonny Apiolaza <rivendell2003@outlook.com>
2026-05-06 21:57:26 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    ACCESS_ID: "Tu_Usuario",
+    ACCESS_SECRET: "Tu_Password",
+    ENDPOINT: "https://openapi.tuyaup.com", // O "https://openapi.tuyaus.com" para America 
+    REGION: "US" 
+};
